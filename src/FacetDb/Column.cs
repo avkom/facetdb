7475cea53d19a62804e4a47f5b6e6e7184f7642b
avkom <avkomq@gmail.com>
2018-01-20 23:23:40 +0300

    Added Column class.

---
+++ src/FacetDb/Column.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FacetDb
+{
+    public class Column
+    {
+		public Type Type { get; private set; }
+
+		public IList List { get; private set; }
+
+		public Column(Type type)
+		{
+			Type = type;
+
+			Type genericListType = typeof(List<>).MakeGenericType(type);
+			List = (IList) Activator.CreateInstance(genericListType);
+		}
+	}
+}
--- src/FacetDb/FacetDatabase.cs
+++ src/FacetDb/FacetDatabase.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FacetDb
 {
     public class FacetDatabase
     {
-	    public void AddColumn(string name, Type type)
+	    private readonly Dictionary<string, Column> _columns;
+
+	    public FacetDatabase()
 	    {
-		    
+		    _columns = new Dictionary<string, Column>();
+	    }
+
+		public void AddColumn(string name, Type type)
+	    {
+		    Column column = new Column(type);
+			_columns.Add(name, column);
 	    }
 
 	    public void AddRow(params object[] values)
 	    {
-		    
+
 	    }
 
 	    public QueryResult QueryRows(Query query)
